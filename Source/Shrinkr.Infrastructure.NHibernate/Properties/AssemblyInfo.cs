498f2717908d60185a5f2121f722231cd94ea830
kazimanzurrashid <kazimanzurrashid@localhost>
2010-04-29 20:56:18 +0000

    Start adding community stack project.

---
+++ Source/Shrinkr.Infrastructure.NHibernate/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+[assembly: AssemblyTitle("Shrinkr.Infrastructure.NHibernate")]
+[assembly: AssemblyProduct("Shrinkr.Infrastructure.NHibernate")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("c10b2b61-2e92-463d-a8fb-dfcfc3996f69")]
+[assembly: InternalsVisibleTo("Shrinkr.Infrastructure.NHibernate.IntegrationTests")]
