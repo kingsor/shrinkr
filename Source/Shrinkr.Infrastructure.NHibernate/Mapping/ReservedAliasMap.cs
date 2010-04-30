cb284799855b52bf8083f9f5b24bd1bc9142967f
kazimanzurrashid <kazimanzurrashid@localhost>
2010-04-30 13:50:55 +0000

    Start writing the NHibernate fluent mapping.

---
+++ Source/Shrinkr.Infrastructure.NHibernate/Mapping/ReservedAliasMap.cs
@@ -0,0 +1,20 @@
+﻿namespace Shrinkr.Infrastructure.NHibernate.Mapping
+{
+    using System;
+
+    using FluentNHibernate.Mapping;
+
+    using DomainObjects;
+
+    [CLSCompliant(false)]
+    public class ReservedAliasMap : ClassMap<ReservedAlias>
+    {
+        public ReservedAliasMap()
+        {
+            Id(b => b.Id).GeneratedBy.Identity();
+            Map(b => b.Name).Not.Nullable().Length(440);
+
+            Table("ReservedAlias");
+        }
+    }
+}
