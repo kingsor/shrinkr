2115b19e708053c6897b7b852985df247a254b3e
Moses <Moses@localhost>
2010-05-02 22:38:16 +0300

    Completed Entities Mapping with Fluent NHibernate (not tested)

---
+++ Source/Shrinkr.Infrastructure.NHibernate/Mapping/VisitMap.cs
@@ -0,0 +1,32 @@
+﻿namespace Shrinkr.Infrastructure.NHibernate.Mapping
+{
+    using System;
+  
+    using FluentNHibernate.Mapping;
+  
+    using DomainObjects;
+
+    [CLSCompliant(false)]
+    public class VisitMap : ClassMap<Visit>
+    {
+        public VisitMap()
+        {
+            Id(v => v.Id).GeneratedBy.Identity();
+
+            Map(v => v.IPAddress).Not.Nullable().Length(15);
+            Map(v => v.Browser).Nullable().Length(440);
+            Map(v => v.GeoCode).Nullable();
+            Map(v => v.CreatedAt).Not.Nullable();
+
+            Component(a => a.Referrer, m =>
+                                           {
+                                               m.Map(r => r.Domain).Nullable().Length(440).Column("ReferrerDomain");
+                                               m.Map(r => r.Url).Nullable().Length(2048).Column("ReferrerUrl");
+                                           });
+
+            References(v => v.Alias).Not.Nullable().Column("AliasId");
+
+            Table("Visit");
+        }
+    }
+}
