540e41cc250dfaae9bdc6f7493d6842ba7291660
Moses <Moses@localhost>
2010-05-04 00:04:08 +0300

    Added and implemented Database, DatabaseFactory DatabaseConfigurationBuilderBase, SqlDatabaseConfigurationBuilder & MySqlDatabaseConfigurationBuilder classes Used Fluent NHibernate for mapping. Modified Visit, User & ShortUrl classes (Marked few propeties as virtual Set default namespace to Shrinkr.Infrastructure.Nhibernate (small h) to be different from already existing NHibernate namespace in NHibernate assembly.

---
+++ Source/Shrinkr.Infrastructure.NHibernate/DatabaseConfigurationBuilder.cs
@@ -0,0 +1,67 @@
+﻿namespace Shrinkr.Infrastructure.Nhibernate
+{
+    using System;
+
+    using NHibernate.Cfg;
+
+    using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
+
+    /// <summary>
+    /// Database configuration class for NHibernate.
+    /// </summary>
+    /// <remarks>
+    /// When using this class with IoC, make sure to configure instances lifetime
+    /// as singleton.
+    /// </remarks>
+    [CLSCompliant(false)]
+    public class DatabaseConfigurationBuilderBase
+    {
+        private readonly Configuration configuration;
+        public Configuration Configuration
+        {
+            get
+            {
+                return configuration;
+            }
+        }
+
+        protected DatabaseConfigurationBuilderBase(IPersistenceConfigurer persistenceConfigurer)
+        {
+            configuration = CreateConfiguraiton(persistenceConfigurer);
+        }
+
+
+        private static Configuration CreateConfiguraiton(IPersistenceConfigurer persistenceConfigurer)
+        {
+            return Fluently.Configure()
+               .Database(persistenceConfigurer)
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Database>())
+                .BuildConfiguration();
+        }
+    }
+
+    /// <summary>
+    /// SQL Server 2008 configuration class for NHibernate.
+    /// </summary>
+    [CLSCompliant(false)]
+    public class SqlDatabaseConfigurationBuilder : DatabaseConfigurationBuilderBase
+    {
+        public SqlDatabaseConfigurationBuilder(string connectionString)
+            : base(MsSqlConfiguration.MsSql2008.ConnectionString(connectionString))
+        {
+        }
+    }
+
+    /// <summary>
+    /// Standard MySQL configuration class for NHibernate.
+    /// </summary>
+    [CLSCompliant(false)]
+    public class MySqlDatabaseConfigurationBuilder : DatabaseConfigurationBuilderBase
+    {
+        public MySqlDatabaseConfigurationBuilder(string connectionString)
+            : base(MySQLConfiguration.Standard.ConnectionString(connectionString))
+        {
+        }
+    }
+}
---
+++ Source/Shrinkr.Infrastructure.NHibernate/IDatabaseFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Shrinkr.Infrastructure.Nhibernate
+{
+    using System;
+
+    public interface IDatabaseFactory : IDisposable
+    {
+        Database Get();
+    }
+}
