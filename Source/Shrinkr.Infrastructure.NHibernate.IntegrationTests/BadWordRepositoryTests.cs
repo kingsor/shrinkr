c0d5c97a1950113fba5addb48e65a068d28255d4
Moses <Moses@localhost>
2010-05-15 01:00:56 +0300

    Completed BadWordRepository with integration testing

--- Source/Shrinkr.Infrastructure.NHibernate.IntegrationTests/BadWordRepositoryTests.cs
+++ Source/Shrinkr.Infrastructure.NHibernate.IntegrationTests/BadWordRepositoryTests.cs
@@ -5,6 +5,7 @@
     using DomainObjects;
 
     using Xunit;
+    using Xunit.Extensions;
 
     public class BadWordRepositoryTests : DatabaseTestBase
     {
@@ -12,21 +13,21 @@
 
         public BadWordRepositoryTests()
         {
-            repository = new BadWordRepository(DatabaseFactory);
+            repository = new BadWordRepository(DatabaseFactory, new QueryFactory(false));
         }
 
         [Fact]
-        public void Should_be_able_to_add_bad_word()
+        public void Add_should_be_able_to_add_bad_word()
         {
             var badWord = new BadWord {Expression = "F**k"};
 
             repository.Add(badWord);
             
-            Assert.NotEqual(0, badWord.Id);
+            Assert.True(badWord.Id >= 0);
         }
 
         [Fact]
-        public void Should_be_able_to_delete_bad_word()
+        public void Delete_should_be_able_to_delete_bad_word()
         {
             var badWord = new BadWord { Expression = "F**k" };
 
@@ -35,6 +36,22 @@
             repository.Delete(badWord);
             
             Assert.False(Database.BadWords.Any(b => b.Id == badWord.Id));
+        }
+
+        [Theory]
+        [InlineData("A$$", "A$$", true)]
+        [InlineData("A$$", "a$$", true)]
+        [InlineData("A$$", "Ace", false)]
+        [InlineData("A$$", "ace", false)]
+        public void IsMatching_should_return_correct_value(string badword, string expression, bool expected)
+        {
+            var badWord = new BadWord { Expression = badword };
+            
+            repository.Add(badWord);
+
+            bool ismatched = repository.IsMatching(expression);
+
+            Assert.Equal(expected, ismatched);
         }
     }
 }
---
+++ Source/Shrinkr.Infrastructure.NHibernate/Query/QueryBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Shrinkr.Infrastructure.NHibernate.Query
+{
+    /// Reserved for futrue use
+    public abstract class QueryBase<TEntity> : IQuery<TEntity>
+    {
+        public abstract TEntity Execute(Database database);
+    }
+}
