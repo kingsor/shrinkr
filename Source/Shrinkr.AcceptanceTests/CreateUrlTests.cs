58bb3e275f2d312e7e9d586305d84214d8c1dfe8
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2010-05-16 03:10:44 +0600

    With alias tests added

--- Source/Shrinkr.AcceptanceTests/CreateUrlTests.cs
+++ Source/Shrinkr.AcceptanceTests/CreateUrlTests.cs
@@ -5,37 +5,68 @@ namespace Shrinkr.AcceptanceTests
     using WatiN.Core;
     using Xunit;
 
-    public class When_short_url_is_created_without_alias : IDisposable
+    public abstract class UrlCreatedAsAnonymousUser : IDisposable
     {
-        private readonly Browser browser = TestHelper.CreateBrowser();
-
-        public When_short_url_is_created_without_alias()
+        protected UrlCreatedAsAnonymousUser()
         {
-            browser.GoTo(TestHelper.ApplicationUrl);
-            browser.TextField(Find.ByName("url")).Value = "http://www.dotnetrocks.com/default.aspx?showNum=1";
-            browser.Button(Find.ByValue("Shrink")).Click();
+            Browser = TestHelper.CreateBrowser();
 
-            browser.WaitForComplete();
+            Browser.GoTo(TestHelper.ApplicationUrl);
+            SetFormValues();
+
+            var submitButton = Browser.Button(Find.ByValue("Shrink"));
+
+            submitButton.Click();
+            submitButton.WaitUntil(btn => btn.Enabled);
+        }
+
+        protected Browser Browser
+        {
+            get;
+            private set;
         }
 
         public void Dispose()
         {
-            browser.Close();
-            browser.Dispose();
+            Browser.Close();
+            Browser.Dispose();
         }
 
         [Fact]
         public void Should_contain_the_success_message()
         {
-            var message = browser.Div(Find.ByClass("messageBox")).Elements[0].Text;
+            var message = Browser.Div(Find.ByClass("messageBox")).Elements[0].Text;
 
             Assert.Equal(message, "Your new urls are generated successfully.");
+        }
+
+        protected abstract void SetFormValues();
+
+        protected void SetFormValues(string url, string alias)
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                Browser.TextField(Find.ByName("url")).Value = url;
+            }
+
+            if (!string.IsNullOrEmpty(alias))
+            {
+                Browser.TextField(Find.ByName("alias")).Value = alias;
+            }
+        }
+    }
+
+    public class When_short_url_is_created_without_alias : UrlCreatedAsAnonymousUser
+    {
+        protected override void SetFormValues()
+        {
+            SetFormValues(string.Format("http://www.dotnetrocks.com/default.aspx?showNum={0}", new Random().Next(1, 558)), null);
         }
 
         [Fact]
         public void Should_contain_the_shrinked_url()
         {
-            var shrinkedUrl = browser.TextField(Find.ById("newUrl")).Value;
+            var shrinkedUrl = Browser.TextField(Find.ById("newUrl")).Value;
 
             Assert.True(shrinkedUrl.StartsWith(TestHelper.ApplicationUrl + "/"));
         }
@@ -43,9 +74,48 @@ namespace Shrinkr.AcceptanceTests
         [Fact]
         public void Should_contain_the_preview_url()
         {
-            var shrinkedUrl = browser.TextField(Find.ById("previewUrl")).Value;
+            var shrinkedUrl = Browser.TextField(Find.ById("previewUrl")).Value;
 
             Assert.True(shrinkedUrl.StartsWith(TestHelper.ApplicationUrl + "/p/"));
+        }
+    }
+
+    public class When_short_url_is_created_with_alias : UrlCreatedAsAnonymousUser
+    {
+        private readonly string alias = CreateRandomAlias();
+
+        [Fact]
+        public void Should_contain_the_shrinked_url()
+        {
+            var shrinkedUrl = Browser.TextField(Find.ById("newUrl")).Value;
+
+            Assert.Equal(TestHelper.ApplicationUrl + "/" + alias, shrinkedUrl);
+        }
+
+        [Fact]
+        public void Should_contain_the_preview_url()
+        {
+            var shrinkedUrl = Browser.TextField(Find.ById("previewUrl")).Value;
+
+            Assert.Equal(TestHelper.ApplicationUrl + "/p/" + alias, shrinkedUrl);
+        }
+
+        protected override void SetFormValues()
+        {
+            SetFormValues(string.Format("http://www.dotnetrocks.com/default.aspx?showNum={0}", new Random().Next(1, 558)), alias);
+        }
+
+        private static string CreateRandomAlias()
+        {
+            var rnd = new Random();
+            var builder = string.Empty;
+
+            for (int i = 1; i <= rnd.Next(2, 24); i++)
+            {
+                builder += ((char)rnd.Next(65, 90)).ToString();
+            }
+
+            return builder;
         }
     }
 }
