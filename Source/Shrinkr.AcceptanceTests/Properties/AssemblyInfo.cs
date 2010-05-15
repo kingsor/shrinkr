626a074e29d3905d938bc512c3912fefa8483202
Kazi Manzur Rashid <kazimanzurrashid@gmail.com>
2010-05-15 22:57:25 +0600

    Started implementing acceptance tests with WaitN.

---
+++ Source/Shrinkr.AcceptanceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Shrinkr.AcceptanceTests")]
+[assembly: AssemblyTitle("Shrinkr.AcceptanceTests")]
+[assembly: Guid("61a09337-b9b3-4249-b986-13361c221317")]
